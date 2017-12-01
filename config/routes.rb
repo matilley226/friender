6c8a6c32e66b0f70dcd12796c8df3281baf59619
Raghu Betina <raghu@firstdraft.com>
2017-12-01 04:29:17 +0000

    Generate activities resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Activity resource:
+  # CREATE
+  get "/activities/new", :controller => "activities", :action => "new"
+  post "/create_activity", :controller => "activities", :action => "create"
+
+  # READ
+  get "/activities", :controller => "activities", :action => "index"
+  get "/activities/:id", :controller => "activities", :action => "show"
+
+  # UPDATE
+  get "/activities/:id/edit", :controller => "activities", :action => "edit"
+  post "/update_activity/:id", :controller => "activities", :action => "update"
+
+  # DELETE
+  get "/delete_activity/:id", :controller => "activities", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
