cd4fa59e6520618dace896047cb79c28d7d15121
Raghu Betina <raghu@firstdraft.com>
2017-12-01 04:29:55 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "activities#index"
   # Routes for the Membership resource:
   # CREATE
